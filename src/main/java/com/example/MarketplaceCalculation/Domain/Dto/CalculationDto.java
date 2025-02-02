6dd143df908debf52077ae5b0dbf5b389067e6b1
CarijaA <carijogamerbr@outlook.com>
2025-02-02 16:56:40 -0300

    Mudanças Básica e adição do README

--- src/main/java/com/example/MarketplaceCalculation/Domain/Dto/CalculationDto.java
+++ src/main/java/com/example/MarketplaceCalculation/Domain/Dto/CalculationDto.java
@@ -3,16 +3,17 @@ package com.example.MarketplaceCalculation.Domain.Dto;
 import com.example.MarketplaceCalculation.Domain.Entity.Enum.MarketplacesNomes;
 import jakarta.validation.constraints.NotNull;
 
-public record CalculationDto(MarketplacesNomes marketplaceNome,
-                             @NotNull int  tipoVendedor,
-                             @NotNull int regiao,
-                             @NotNull int  tipoEnvio,
-                             @NotNull double custoProduto,
-                             @NotNull double precoVenda,
-                             @NotNull double promocaoVenda,
-                             @NotNull double precoVendaLiquida,
-                             @NotNull double taxaFixa, // produtos abaixo de 79,00 pagam R$6,00
-                             @NotNull double marketplaceTaxa,
-                             @NotNull double notaFiscalTaxa,
-                             @NotNull double pesoProduto) {
+public record CalculationDto(
+        MarketplacesNomes marketplaceNome,
+        @NotNull int reputacao, // 1- Verde, 2- Amarelo, 3- Vermelho
+        @NotNull int regiao, // 1- Sudeste, 2- Demais
+        @NotNull int tipoEnvio, // 1- Full, 2- Outros
+        @NotNull double custoProduto,
+        @NotNull double precoVenda,
+        @NotNull double promocaoVenda,
+        @NotNull double taxaFixa,
+        @NotNull double marketplaceTaxa,
+        @NotNull double notaFiscalTaxa,
+        @NotNull double pesoProduto
+) {
 }
